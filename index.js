beb16cd7f5ebb8f382bcb65fe47f8a5c2bf99748
Ebi Subian <96483950+EbiSubian46@users.noreply.github.com>
2022-09-19 22:45:19 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+
+var numberOfButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButton ; i++) {
+document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+        var buttoninnerHTML = this.innerHTML;
+        keyboardButton(buttoninnerHTML);
+        animasiTombol (buttoninnerHTML);
+        
+});
+
+}
+
+document.addEventListener ("keypress", function(event){
+
+        keyboardButton(event.key);
+        animasiTombol(event.key);
+});
+
+function keyboardButton (key){
+
+        switch (key) {
+                case "w":
+                        var tom1 = new Audio("sounds/tom-1.mp3");
+                        tom1.play();
+                        break;
+                case "a":
+                        var tom2 = new Audio("sounds/tom-2.mp3");
+                        tom2.play();
+                        break;
+                case "s":
+                        var tom3 = new Audio ("sounds/tom-3.mp3");
+                        tom3.play();
+                        break;
+                case "d":
+                        var tom4 = new Audio ("sounds/tom-4.mp3");
+                        tom4.play();
+                        break;
+                case "j":
+                        var snare = new Audio("sounds/snare.mp3");
+                        snare.play();
+                        break;
+                case "k":
+                        var crash = new Audio ("sounds/crash.mp3");
+                        crash.play();
+                        break;
+                case "l":
+                        var kickBass = new Audio ("sounds/kick-bass.mp3");
+                        kickBass.play();
+                        break;
+                default:
+                        console.log(key);
+
+        }
+
+}
+
+function animasiTombol (kunciSebelumnya) {
+
+        var tombolKlik = document.querySelector("." +kunciSebelumnya);
+
+        tombolKlik.classList.add("pressed");
+
+        setTimeout(function() {
+                tombolKlik.classList.remove("pressed");
+        }, 100);
+
+}
